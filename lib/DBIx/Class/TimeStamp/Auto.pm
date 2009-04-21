d56ec7e4229a4fd6616d236cea2d9cd0237a58cf
kamipo <kamipo@gmail.com>
2009-04-21 16:57:29 +0900

    initial commit

---
+++ lib/DBIx/Class/TimeStamp/Auto.pm
@@ -0,0 +1,33 @@
+package DBIx::Class::TimeStamp::Auto;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Class::TimeStamp::Auto -
+
+=head1 SYNOPSIS
+
+  use DBIx::Class::TimeStamp::Auto;
+
+=head1 DESCRIPTION
+
+DBIx::Class::TimeStamp::Auto is
+
+=head1 AUTHOR
+
+Ryuta Kamizono E<lt>kamipo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
