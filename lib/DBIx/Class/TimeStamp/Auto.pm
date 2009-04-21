f8dd3cf958984240c1a2fc99063d756c25bb0fb3
kamipo <kamipo@gmail.com>
2009-04-21 20:09:21 +0900

    first commit

--- lib/DBIx/Class/TimeStamp/Auto.pm
+++ lib/DBIx/Class/TimeStamp/Auto.pm
@@ -4,6 +4,58 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use base 'DBIx::Class::InflateColumn::DateTime::Auto';
+use DBIx::Class::ResultSet ();
+
+sub import {
+    my $class = shift;
+    my %columns = ref $_[0] eq 'HASH' ? %{$_[0]} : @_;
+
+    if (my $create = $columns{auto_create_datetime_columns}) {
+        $create = [$create] if ref $create ne 'ARRAY';
+        $class->_auto_create_datetime_columns($create);
+    }
+
+    if (my $update = $columns{auto_update_datetime_columns}) {
+        $update = [$update] if ref $update ne 'ARRAY';
+        $class->_auto_update_datetime_columns($update);
+    }
+
+    if (my $timezone = $columns{datetime_timezone}) {
+        $class->datetime_timezone($timezone);
+    }
+}
+
+package DBIx::Class::ResultSet;
+
+use Class::Method::Modifiers;
+
+__PACKAGE__->mk_classdata(timestamp_class => 'DBIx::Class::TimeStamp::Auto');
+
+before update => sub {
+    my ($self, $attrs) = @_;
+
+    return if ref $attrs ne 'HASH';
+
+    my $now;
+    my $timestamp_class = $self->timestamp_class;
+
+    for (@{$timestamp_class->auto_update_datetime_columns}) {
+        $attrs->{$_} = $now ||= $timestamp_class->get_current_datetime
+            if !defined $attrs->{$_} and $self->is_inflate_datetime_column($_);
+    }
+};
+
+sub is_inflate_datetime_column {
+    my ($self, $column) = @_;
+
+    my $source = $self->result_source;
+    my $info   = $source->column_info($column) if $source->has_column($column);
+
+    return 1 if $info->{data_type} =~ /^(?:date(?:time)?|timestamp)$/i
+            and $info->{_inflate_info};
+}
+
 1;
 __END__
 
